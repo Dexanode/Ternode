f92f17947b48b41c927e2ef3f47629bb42377b21
0xDexa <65535542+nadi555@users.noreply.github.com>
2023-03-21 02:01:42 +0700

    Add files via upload

---
+++ bonusblock/README.md
@@ -0,0 +1,213 @@
+
+# Guide Intsallation Node Validator BonusBlock 
+
+<p style="font-size:14px" align="left">
+<a href="https://t.me/airdropsultanindonesia" target="_blank">Join to Channel Airdrop Sultan Indonesia</a>
+</p>
+
+<p align="center">
+  <img style="margin: auto; height: 100px; border-radius: 50%;" src="https://user-images.githubusercontent.com/65535542/226438691-a9abb0cb-4eb0-439b-a434-5e1eb87bc4a0.png">
+</p>
+
+
+# Official Links
+### [Official Document](https://docs.bonus.network/validators/overview.html)
+### [bonus Official Discord](https://discord.gg/cwmR8jNrNp)
+
+# Explorer
+### [Explorer](https://explorer.bonus.network/blocktopia-1/staking)
+
+## Minimum Requirements 
+- 4 or more physical CPU cores
+- At least 500GB of SSD disk storage
+- At least 16GB of memory (RAM)
+- At least 120mbps network bandwidth
+
+# Automatic Install Node Guide
+
+wget -O bonusbx.sh https://<raw class="github"></raw>usercontent.com/ternode/tree/main/bonusblock/bonus.sh && chmod +x bonus.sh && ./bonus.sh
+
+## Snapshoot
+
+```bash
+Soon
+```
+
+
+## Cheatsheet
+
+### Create wallet
+To create new wallet use 
+```
+bonus-blockd keys add wallet
+```
+Change `wallet` to your wallet name
+
+To recover existing keys use 
+```
+bonus-blockd keys add wallet --recover
+```
+Change `wallet` to your wallet name
+
+To see current keys 
+```
+bonus-blockd keys list
+```
+
+```
+
+### Create validator
+After your node is synced, create validator
+
+To check if your node is synced simply run
+`curl http://localhost:111657/status sync_info "catching_up": false`
+
+Creating validator with `1 bonus` change the value as you like
+
+```
+bonus-blockd tx staking create-validator \
+  --amount 100000ubonus \
+  --from $WALLET \
+  --commission-max-change-rate "0.01" \
+  --commission-max-rate "0.2" \
+  --identity= "" \
+  --website= ""\
+  --commission-rate "0.07" \
+  --min-self-delegation "1" \
+  --pubkey  $(bonus-blockd tendermint show-validator) \
+  --moniker $NODENAME \
+  --gas=auto \
+  --gas-adjustment=1.2 \
+  --gas-prices=0.025ubonus \
+  --chain-id $BONUS_CHAIN_ID \
+  --node tcp://127.0.0.1:17657 \
+  -- yes
+```
+
+## Usefull commands
+### Service management
+Check logs
+```
+journalctl -fu bonus-blockd -o cat
+```
+
+Start service
+```
+sudo systemctl start bonus-blockd
+```
+
+Stop service
+```
+sudo systemctl stop bonus-blockd
+```
+
+Restart service
+```
+sudo systemctl restart bonus-blockd
+```
+
+### Node info
+Synchronization info
+```
+bonus-blockd status 2>&1 | jq .SyncInfo
+```
+
+Validator info
+```
+bonus-blockd status 2>&1 | jq .ValidatorInfo
+```
+
+Node info
+```
+bonus-blockd status 2>&1 | jq .NodeInfo
+```
+
+Show node id
+```
+bonus-blockd tendermint show-node-id
+```
+
+### Wallet operations
+List of wallets
+```
+bonus-blockd keys list
+```
+
+Recover wallet
+```
+bonus-blockd keys add wallet --recover
+```
+
+Delete wallet
+```
+bonus-blockd keys delete wallet
+```
+
+Get wallet balance
+```
+bonus-blockd query bank balances <address>
+```
+
+Transfer funds
+```
+bonus-blockd tx bank send <FROM ADDRESS> <TO_bonus_WALLET_ADDRESS> 10000000abonus
+```
+
+### Voting
+```
+bonus-blockd tx gov vote 1 yes --from wallet --chain-id=blocktopia-1
+```
+
+### Staking, Delegation and Rewards
+Delegate stake
+```
+bonus-blockd tx staking delegate <bonus valoper> 10000000abonus --from=wallet --chain-id=blocktopia-1 --gas=auto
+```
+
+Redelegate stake from validator to another validator
+```
+bonus-blockd tx staking redelegate <srcValidatorAddress> <destValidatorAddress> 10000000abonus --from=wallet --chain-id=blocktopia-1 --gas=auto
+```
+
+Withdraw all rewards
+```
+bonus-blockd tx distribution withdraw-all-rewards --from=wallet --chain-id=blocktopia-1 --gas=auto
+```
+
+Withdraw rewards with commision
+```
+bonus-blockd tx distribution withdraw-rewards <bonus valoper> --from=wallet --commission --chain-id=blocktopia-1
+```
+
+### Validator management
+Edit validator
+```
+bonus-blockd tx staking edit-validator \
+  --moniker=$MONIKER \
+  --identity=<your_keybase_id> \
+  --website="<your_website>" \
+  --details="<your_validator_description>" \
+  --chain-id=blocktopia-1 \
+  --from=wallet
+```
+
+Unjail validator
+```
+bonus-blockd tx slashing unjail \
+  --broadcast-mode=block \
+  --from=wallet \
+  --chain-id=blocktopia-1 \
+  --gas=auto
+```
+
+### Delete node
+```
+sudo systemctl stop bonus-blockd && \
+sudo systemctl disable bonus-blockd && \
+rm /etc/systemd/system/bonus-blockd.service && \
+sudo systemctl daemon-reload && \
+cd $HOME && \
+rm -rf .bonusblock && \
+rm -rf $(which bonus-blockd)
+```
+
---
+++ bonusblock/bonus.sh
@@ -0,0 +1,165 @@
+
+echo -e "\033[0;36m"
+echo "		   / __ \___  _  ______ _____  ____  ____/ /__    ";
+echo "		  / / / / _ \| |/_/ __ `/ __ \/ __ \/ __  / _ \	  ";
+echo "		 / /_/ /  __/>  </ /_/ / / / / /_/ / /_/ /  __/   ";
+echo "		/_____/\___/_/|_|\__,_/_/ /_/\____/\__,_/\___/    ";
+echo ">>> BonusBlock Auto Installation | Chain ID : blocktopia-1  <<<";
+echo -e "\e[0m"
+
+sleep 2
+
+# Variable
+SOURCE=BonusBlock-chain
+WALLET=wallet
+BINARY=bonus-blockd 
+CHAIN=blocktopia-01
+FOLDER=.bonusblock
+VERSION=7c7522c2879ddeaa4ca72d7c1367d18a96d41741
+DENOM=ubonus
+COSMOVISOR=cosmovisor
+REPO=https://github.com/BBlockLabs/BonusBlock-chain
+GENESIS=https://raw.githubusercontent.com/sxlzptprjkt/resource/master/testnet/bonusblock/genesis.json
+ADDRBOOK=https://raw.githubusercontent.com/sxlzptprjkt/resource/master/testnet/bonusblock/addrbook.json
+PORT=111
+
+# Set Vars
+if [ ! $NODENAME ]; then
+        read -p "hello@nodexcapital:~# [ENTER YOUR NODENAME] > " NODENAME
+        echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
+fi
+
+echo "Verify the information below before proceeding with the installation!"
+echo ""
+echo -e "NODE NAME      : \e[1m\e[36m$NODENAME\e[0m"
+echo -e "WALLET NAME    : \e[1m\e[36m$WALLET\e[0m"
+echo -e "CHAIN NAME     : \e[1m\e[36m$CHAIN\e[0m"
+echo -e "NODE VERSION   : \e[1m\e[36m$VERSION\e[0m"
+echo -e "NODE FOLDER    : \e[1m\e[36m$FOLDER\e[0m"
+echo -e "NODE DENOM     : \e[1m\e[36m$DENOM\e[0m"
+echo -e "NODE ENGINE    : \e[1m\e[36m$COSMOVISOR\e[0m"
+echo -e "SOURCE CODE    : \e[1m\e[36m$REPO\e[0m"
+echo -e "NODE PORT      : \e[1m\e[36m$PORT\e[0m"
+echo ""
+
+read -p "Is the above information correct? (y/n) " choice
+if [[ $choice == [Yy]* ]]; then
+
+echo "export SOURCE=${SOURCE}" >> $HOME/.bash_profile
+echo "export WALLET=${WALLET}" >> $HOME/.bash_profile
+echo "export BINARY=${BINARY}" >> $HOME/.bash_profile
+echo "export DENOM=${DENOM}" >> $HOME/.bash_profile
+echo "export CHAIN=${CHAIN}" >> $HOME/.bash_profile
+echo "export FOLDER=${FOLDER}" >> $HOME/.bash_profile
+echo "export VERSION=${VERSION}" >> $HOME/.bash_profile
+echo "export COSMOVISOR=${COSMOVISOR}" >> $HOME/.bash_profile
+echo "export REPO=${REPO}" >> $HOME/.bash_profile
+echo "export GENESIS=${GENESIS}" >> $HOME/.bash_profile
+echo "export ADDRBOOK=${ADDRBOOK}" >> $HOME/.bash_profile
+echo "export PORT=${PORT}" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+else
+    echo "Installation cancelled!"
+    exit 1
+fi
+
+# Package
+sudo apt -q update
+sudo apt -qy install curl git jq lz4 build-essential
+sudo apt -qy upgrade
+
+# Install GO
+sudo rm -rf /usr/local/go
+curl -Ls https://go.dev/dl/go1.19.7.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
+eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
+
+# Get testnet version of Bonus Block
+cd $HOME
+rm -rf $SOURCE
+git clone $REPO
+cd $SOURCE
+git checkout $VERSION
+make install
+go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.4.0
+
+# Prepare binaries for Cosmovisor
+mkdir -p $HOME/$FOLDER/$COSMOVISOR/genesis/bin
+mv $HOME/go/bin/$BINARY $HOME/$FOLDER/$COSMOVISOR/genesis/bin/
+
+
+# Create application symlinks
+ln -s $HOME/$FOLDER/$COSMOVISOR/genesis $HOME/$FOLDER/$COSMOVISOR/current
+sudo ln -s $HOME/$FOLDER/$COSMOVISOR/current/bin/$BINARY /usr/local/bin/$BINARY
+
+# Init generation
+$BINARY config chain-id $CHAIN
+$BINARY config keyring-backend test
+$BINARY config node tcp://localhost:${PORT}57
+$BINARY init $NODENAME --chain-id $CHAIN
+
+# Set peers and seeds
+SEEDS="e5e04918240cfe63e20059a8abcbe62f7eb05036@bonusblock-testnet-p2p.alter.network:26656"
+PEERS="d7f5483ba2d290a3853b4db5df21579e9c8da69a@peers-bonusblock.sxlzptprjkt.xyz:30656"
+sed -i -e "s|^persistent_peers *=.*|persistent_peers = \"$PEERS\"|" $HOME/$FOLDER/config/config.toml
+sed -i -e "s|^seeds *=.*|seeds = \"$SEEDS\"|" $HOME/$FOLDER/config/config.toml
+
+# Download genesis and addrbook
+curl -Ls $GENESIS > $HOME/$FOLDER/config/genesis.json
+curl -Ls $ADDRBOOK > $HOME/$FOLDER/config/addrbook.json
+
+# Set Port
+sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${PORT}58\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${PORT}57\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${PORT}60\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${PORT}56\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${PORT}60\"%" $HOME/$FOLDER/config/config.toml
+sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${PORT}17\"%; s%^address = \":8080\"%address = \":${PORT}80\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:${PORT}90\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${PORT}91\"%" $HOME/$FOLDER/config/app.toml
+
+# Set Config Pruning
+pruning="custom"
+pruning_keep_recent="100"
+pruning_keep_every="0"
+pruning_interval="10"
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/$FOLDER/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/$FOLDER/config/app.toml
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/$FOLDER/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/$FOLDER/config/app.toml
+
+# Set minimum gas price
+sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.0001$DENOM\"/" $HOME/$FOLDER/config/app.toml
+
+# Enable snapshots
+sed -i -e "s/^snapshot-interval *=.*/snapshot-interval = \"2000\"/" $HOME/$FOLDER/config/app.toml
+$BINARY tendermint unsafe-reset-all --home $HOME/$FOLDER --keep-addr-book
+curl -L https://bonusb-t.service.indonode.net/bonusb-snapshot.tar.lz4 | tar -Ilz4 -xf - -C $HOME/$FOLDER
+
+# Create Service
+sudo tee /etc/systemd/system/$BINARY.service > /dev/null << EOF
+[Unit]
+Description=$BINARY
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which cosmovisor) run start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+Environment="DAEMON_HOME=$HOME/$FOLDER"
+Environment="DAEMON_NAME=$BINARY"
+Environment="UNSAFE_SKIP_BACKUP=true"
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Register And Start Service
+sudo systemctl start $BINARY
+sudo systemctl daemon-reload
+sudo systemctl enable $BINARY
+
+echo -e "\033[0;35m=============================================================\033[0m"
+echo -e "\033[0;35mCONGRATS! SETUP FINISHED\033[0m"
+echo ""
+echo -e "CHECK STATUS BINARY : \023[1m\034[35msystemctl status $BINARY\033[0m"
+echo -e "CHECK RUNNING LOGS : \032[1m\034[35mjournalctl -fu $BINARY -o cat\033[0m"
+echo -e "CHECK LOCAL STATUS : \032[1m\034[35mcurl -s localhost:${PORT}57/status | jq .result.sync_info\033[0m"
+echo -e "\033[0;35m=============================================================\033[0m"
+
+# End
